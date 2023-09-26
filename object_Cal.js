5ca2692b4fa7c12008247420fa0d4db91a801c9b
pcology <ssgkp.singh@gmail.com>
2023-09-26 22:02:45 +0530

    object cal

--- object_Cal.js
+++ object_Cal.js
@@ -1,5 +1,5 @@
 
-let cal = {
+let cal1 = {
     setVal(a, b) {
         this.a = a
         this.b = b
@@ -11,5 +11,32 @@ let cal = {
         return this.a * this.b
     }
 }
-console.log(cal.setVal(2, 8))
-console.log(cal.mul())
+console.log(cal1.setVal(2, 8))
+console.log(cal1.mul())
+
+
+// object  cal.mul(2).add(5)
+
+let cal = {
+    total: 1,
+    add(a) {
+        this.total += a
+        return this
+    },
+    sub(a) {
+        this.total -= a
+        return this
+    },
+    mul(a) {
+        this.total *= a
+        return this
+    },
+    div(a) {
+        this.total /= a
+        return this
+    }
+}
+
+const fn = cal.mul(2).add(5)
+console.log(fn.total)
+
